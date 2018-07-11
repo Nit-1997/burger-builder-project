3ca3ea0192c212df5d4467f57815d47048791dee
nitin <ntnbhat9@gmail.com>
2018-07-11 10:56:41 +0530

    hook in the server

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import axios from 'axios';
 
 class App extends Component {
+ state = {
+    response: ''
+  };
+
+  componentDidMount() {
+  	axios.get( '/api/hello' )
+            .then( response => {
+                 console.log(response,'from backend');
+                //this.setState({});
+            } )
+            .catch(error => {
+                 console.log(error);
+            });
+  }
   render() {
     return (
       <div>
